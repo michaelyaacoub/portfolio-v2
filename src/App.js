09b9dd78cdf41476cd47833eba2269650b86dc24
Michael Yaacoub <michael.yaacoub7@gmail.com>
2024-02-08 17:50:38 -1000

    added header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-
 import './App.css';
 
 function App() {
   return (
     <>
       <header>
-        Michael Yaacoub
+        <div className="nav">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+          </ul>
+        </div>
+        <div className="logo">
+          <h1>M.Y</h1>
+        </div>
+        <div className="media-links">
+          <ul>
+            <li>GitHub</li>
+            <li>LinkedIn</li>
+          </ul>
+        </div>
       </header>
+  <div>{head()}</div>
     </>
+
   );
+  function head(){
+    return(
+      <div className="body">
+        <h1>Hi my name is michael</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
